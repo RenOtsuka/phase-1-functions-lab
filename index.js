c7e8f7133f6ef833cf6b63b147dd2cfabd6bf264
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-01-11 11:08:44 -0600

    commit tree 6023a53044dd6248afab9f0b25a836a291a9d5b4

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    if(street >= 42){
+        return street - 42;
+    }
+    else{
+        return 42 - street;
+    }
+}
+
+function distanceFromHqInFeet(street){
+    if(street >= 42){
+        return (street - 42) * 264;
+    }
+    else{
+        return (42 - street) * 264;
+    }
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(start >= destination){
+        return (start - destination) * 264;
+    }
+    else{
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+
+    if(distance < 400){
+        return 0;
+    }
+    else if(distance < 2000){
+        distance -= 400;
+        return distance * 0.02;
+    }
+    else if(distance < 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+}
